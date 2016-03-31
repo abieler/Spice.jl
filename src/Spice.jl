39fcb4f3d67c772729264f36af2fc8ec77691797
abieler <abieler@umich.edu>
2016-03-31 18:55:02 -0400

    replace ASCIIString by AbstractString

--- src/Spice.jl
+++ src/Spice.jl
@@ -47,26 +47,26 @@ end
 
 function str2et(s::AbstractString)
   et = Ref{Cdouble}(0.0)
+  ccall((:str2et_c, sharedLib), Float64, (Ptr{Cchar}, Ptr{Cdouble}), s, et)
+  return et[]
+end
+
+function utc2et(s::AbstractString)
+  et = Ref{Cdouble}(0.0)
   ccall((:utc2et_c, sharedLib), Float64, (Ptr{Cchar}, Ptr{Cdouble}), s, et)
   return et[]
 end
 
-function utc2et(s::ASCIIString)
-  et = Ref{Cdouble}(0.0)
-  ccall((:utc2et_c, sharedLib), Void, (Ptr{Cchar},Ptr{Cdouble}), s, et)
-  return et[]
-end
 
-
-function scs2e(sc::Int64, sclkch::ASCIIString)
+function scs2e(sc::Int64, sclkch::AbstractString)
   et = Ref{Cdouble}(0.0)
   ccall((:scs2e_c, sharedLib), Void, (Int64, Ptr{Cchar}, Ptr{Cdouble}),
         sc, sclkch, et)
   return et[]
 end
 
-function spkpos(target::ASCIIString, et::Float64, ref::ASCIIString,
-                abcorr::ASCIIString, obs::ASCIIString)
+function spkpos(target::AbstractString, et::Float64, ref::AbstractString,
+                abcorr::AbstractString, obs::AbstractString)
   pos = zeros(Float64, 3)
   lt = Ref{Cdouble}(0.0)
   ccall((:spkpos_c, sharedLib), Void, (Ptr{Cchar}, Float64,
@@ -75,18 +75,18 @@ function spkpos(target::ASCIIString, et::Float64, ref::ASCIIString,
   return pos, lt[]
 end
 
-function timout(et::Float64, pictur::ASCIIString, lenOutput::Int64)
+function timout(et::Float64, pictur::AbstractString, lenOutput::Int64)
   s = "............................."
   ccall((:timout_c, sharedLib), Void, (Float64, Ptr{Cchar}, Int64, Ptr{Cchar}),
         et, pictur, lenOutput, s)
   return s
 end
 
-function unload(KernelFile::ASCIIString)
+function unload(KernelFile::AbstractString)
   ccall((:furnsh_c, sharedLib),Void,(Ptr{Cchar},),KernelFile)
 end
 
-function utc2et(s::ASCIIString)
+function utc2et(s::AbstractString)
   et = 0.0
   ccall((:utc2et_c, sharedLib), Float64, (Ptr{Cchar}, Float64), s, et)
 end
